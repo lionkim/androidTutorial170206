f36d4cbed48bf5b89201d41cf7ccb7e27809a756
lionkim <cppcode@naver.com>
2017-02-07 10:19:39 +0900

    action item

---
+++ 02_widget2/app/src/main/java/com/example/cppco/a02_widget2/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.cppco.a02_widget2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Checkable;
+import android.widget.CompoundButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        CheckBox checkBox = (CheckBox) findViewById(R.id.myCheckBox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "myCheckBox : " + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button button = (Button) findViewById(R.id.myButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        RadioGroup radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.radio1:
+                        Toast.makeText(MainActivity.this, "radio1 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case  R.id.radio2:
+                        Toast.makeText(MainActivity.this, "radio2 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                    case  R.id.radio3:
+                        Toast.makeText(MainActivity.this, "radio3 selected", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
+    }
+}
